7ba08a02c308feed5624ca5d6e18362fb5dd9804
gurevaleksey <gurevaleksey@yandex.ru>
2021-03-21 15:45:31 +0300

    создание проекта

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let questions = {
+  question1 : {
+    question : '',
+    answer : '',
+    currentAnswer : ''
+  },
+  question2 : {
+    question : '',
+    answer : '',
+    currentAnswer : ''
+  },
+  question3 : {
+    question : '',
+    answer : '',
+    currentAnswer : ''
+  },
+  question4 : {
+    question : '',
+    answer : '',
+    currentAnswer : ''
+  },
+}
+
